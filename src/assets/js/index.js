46d29a2400cb6dca55659b2c84d7615264542a9c
shketik-r <ruslans3101@gmail.com>
2024-02-14 11:24:57 +0300

    add new page

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -409,6 +409,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 
 
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+
   /*-----------------------*/
 
 
